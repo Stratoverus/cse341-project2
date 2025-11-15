4ecab9797b41b2f4c288a8c758d57a63171aed25
Stratoverus <keitheberhard93@gmail.com>
2025-11-14 22:06:39 -0700

    Updates to the swagger file, to make it so that the localhost isn't an issue on render.

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger-output.json');
 
+if (process.env.HOST) {
+  swaggerDocument.host = process.env.HOST;
+} else {
+  delete swaggerDocument.host;
+}
+
 router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 module.exports = router;
